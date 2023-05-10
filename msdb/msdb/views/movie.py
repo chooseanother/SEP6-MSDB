40366196d0b912408b72d704a4d84f6235614c6d
Maggie <maki.lapcik@gmail.com>
2023-05-10 15:24:49 +0200

    Set up API calls, sending information till frontend, displaying data incl. poster in a simple way

--- msdb/msdb/views/movie.py
+++ msdb/msdb/views/movie.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+
+from msdb.models import Movie
+from msdb.utils.movie_api import get_movie_from_api, MovieFromApi
+
+
 def movie(request, movie_id):
-    #movie = Movie.objects.get(id=movie_id)
-    movie = dict(title="Kill Bill Vol. 1", id="tt826471")
-    context = dict(movie=movie)
-    return render(request, "movies/movies.html", context)
+    try:
+        #movie = Movie.objects.get(id=movie_id)
+        movie = get_movie_from_api(movie_id)
+    except Movie.DoesNotExist:
+        return render(request, "404.html", dict(movie_id=movie_id))
+
+    if isinstance(movie, MovieFromApi):
+        context = dict(movie=movie)
+        print(context)
+        return render(request, "movies/movies.html", context)
+    else:
+        return render(request, "404.html", dict(exception=movie))
+
+
+
+
