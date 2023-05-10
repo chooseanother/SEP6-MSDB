dd28a2dc801e62354d9ed5e5bcd5aebb094f2f2e
Maggie <maki.lapcik@gmail.com>
2023-05-10 12:19:36 +0200

    set up Admin for Movie

---
+++ msdb/msdb/admin/movie.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from msdb.models import Movie
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "year",
+    ]
