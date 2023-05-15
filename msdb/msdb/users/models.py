d07d93deebbc8704ed3c31aec694aefc8ace2c16
Kim Tranberg <mail@kimdt.tech>
2023-05-15 15:25:21 +0200

    Made user.name unique

--- msdb/msdb/users/models.py
+++ msdb/msdb/users/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
     """
 
     # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = CharField(_("Name of User"), blank=True, max_length=255, unique=True)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
     email = EmailField(_("email address"), unique=True)
