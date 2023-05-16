7ce51f975d753853a6150479c1b7d4f4df6706ad
Kim Tranberg <mail@kimdt.tech>
2023-05-16 09:36:24 +0200

    Added review model class, migration and admin

---
+++ msdb/msdb/models/review.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Review(models.Model):
+    # Rating choices from 1 to 5
+    RATING_CHOICES = [(i, f'{i}') for i in range(1, 6)]
+
+    movie = models.ForeignKey("Movie", on_delete=models.CASCADE, related_name="reviews", null=False, blank=False)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="reviews", null=False, blank=False)
+    rating = models.IntegerField(choices=RATING_CHOICES, null=False, blank=False)
+    review = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
