7f087eff3369ff27902d2e051c828d541073ad86
Kim Tranberg <mail@kimdt.tech>
2023-05-16 15:27:01 +0200

    Updated movie model

--- msdb/msdb/models/movie.py
+++ msdb/msdb/models/movie.py
@@ -1,10 +1,14 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 
 class Movie(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     title = models.CharField(max_length = 255)
     year = models.PositiveIntegerField()
+    genre = ArrayField(models.CharField(max_length=255), blank=True, null=True)
+    director = ArrayField(models.CharField(max_length=255), blank=True, null=True)
+    actors = ArrayField(models.CharField(max_length=255), blank=True, null=True)
+    poster_url = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         # This model is created by .sql script and is not managed by Django
