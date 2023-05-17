b1379b208b9319a6cd9b3164a26eb376415da565
Kim Tranberg <mail@kimdt.tech>
2023-05-17 14:57:50 +0200

    DB updated with genre, director, actors and poster

--- msdb/update_movies.py
+++ msdb/update_movies.py
@@ -1,26 +1,32 @@
 from msdb.models import Movie
 from msdb.utils.movie_api import MovieFromApi, get_movie_from_api
 
-#API_KEY = "e6f34a57"
-#URL = "http://www.omdbapi.com/" #?apikey=e6f34a57&i=tt3896198
 
-MOVIE_ID = "tt3896198"
+# script to update moveis with more information from api
+def update_movies(number : int = 1000, offset : int = 0):
+    # get movies that needs to be updated
+    movies = Movie.objects.filter(genre__isnull=True)[offset*number:number*offset+number]
 
-def main():
+    updated = 0
+    not_updated = 0
 
-    api_movie = get_movie_from_api(MOVIE_ID)
-    
-    local_movie = Movie.objects.get(id=MOVIE_ID)
+    for movie in movies:
+        print(f'Updating movie {movie.id}')
+        api_movie = get_movie_from_api(movie.id)
 
-    print(api_movie)
-    print(local_movie)
+        if isinstance(api_movie, MovieFromApi):
+            movie.genre = api_movie.genre
+            movie.director = api_movie.director
+            movie.actors = api_movie.actors
+            movie.poster_url = api_movie.poster_link
+            movie.save()
+            updated += 1
+            print(f'Movie {movie.id} updated. total {updated} of {number}')
+        else:
+            not_updated += 1
+            print(f'Movie {movie.id} not updated. total {not_updated} of {number}')
 
-    if isinstance(api_movie, MovieFromApi):
-        local_movie.genre = api_movie.genre
-        local_movie.director = api_movie.director
-        local_movie.actors = api_movie.actors
-        local_movie.poster_url = api_movie.poster_link
+    count = Movie.objects.filter(genre__isnull=True).count()
 
+    print(f'{count} movies left. updated: {updated} not updated: {not_updated}')
 
-if __name__ == "__main__":
-    main()
