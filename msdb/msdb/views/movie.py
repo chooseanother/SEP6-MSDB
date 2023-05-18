65dd31b1b8ba555ea3fa04e75ba48ef574f3f7f4
Maggie <maki.lapcik@gmail.com>
2023-05-18 16:06:26 +0200

    Updated movie page to use from our database as much as possible

--- msdb/msdb/views/movie.py
+++ msdb/msdb/views/movie.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 
 from msdb.utils.movie_api import get_movie_from_api, MovieFromApi
+from msdb.models import Movie
 
 def movie(request, movie_id):
-    movie = None
+    #if movie id does not exist in our database, throw 404
     try:
-        movie = get_movie_from_api(movie_id)
+        movie_datab = Movie.objects.get(id=movie_id)
+    except Movie.DoesNotExist:
+        return render(request, "404.html", dict(movie_id=movie_id))
+
+    #if movie exists, try getting more information from api
+    #if api throws an exception, we just display what we have
+    movie_api = None
+    try:
+        movie_api = get_movie_from_api(movie_id)
     except Exception:
         pass
 
-    if isinstance(movie, MovieFromApi):
-        context = dict(movie=movie)
-        return render(request, "movies/movie.html", context)
-    else:
-        return render(request, "404.html", dict(exception=movie))
+    context = dict(movie_api=movie_api, movie_datab=movie_datab)
+    return render(request, "movies/movie.html", context)
 
 
 
