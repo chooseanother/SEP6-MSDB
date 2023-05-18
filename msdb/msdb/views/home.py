abecf2e8916b2ff2e437279b4f719d03ea0ab18f
Maggie <maki.lapcik@gmail.com>
2023-05-18 11:16:59 +0200

    Home - hot stuff

--- msdb/msdb/views/home.py
+++ msdb/msdb/views/home.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import datetime
+
+from msdb.models import Movie
+
 def home(request):
-    list = {1, 2, 3, 4, 6, 8}
-    context = dict(list=list)
+    current_year =datetime.date.today().year
+    hot_stuff = Movie.objects.filter(year=current_year).filter(poster_url__isnull=False)[:5]
+    top_10 = Movie.objects.filter(director='Tim Burton')[:10]
+
+    context = dict(hot_stuff=hot_stuff, top_10=top_10)
     return render(request, "movies/movies_home.html", context)
