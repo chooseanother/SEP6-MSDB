7479425753bb2a3cd37e456f79acc9717c8c9016
Maggie <maki.lapcik@gmail.com>
2023-05-18 14:49:17 +0200

    Home - top 10 Updated css rules (search, home)

--- msdb/msdb/views/home.py
+++ msdb/msdb/views/home.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 import datetime
+import random
 
 from msdb.models import Movie
 
 def home(request):
     current_year =datetime.date.today().year
     hot_stuff = Movie.objects.filter(year=current_year).filter(poster_url__isnull=False)[:5]
-    top_10 = Movie.objects.filter(director='Tim Burton')[:10]
+    top_10 = Movie.objects.filter(director__icontains='Tim Burton')[:10]
 
-    context = dict(hot_stuff=hot_stuff, top_10=top_10)
+    context = dict(hot_stuff=hot_stuff, top_10=top_10, current_year=current_year)
     return render(request, "movies/movies_home.html", context)
