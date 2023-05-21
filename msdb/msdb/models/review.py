dd5007f92cd3b8003ceacd3f6a62d7ce1a9cffad
Kim Tranberg <mail@kimdt.tech>
2023-05-21 12:03:51 +0200

    restructuring review template

--- msdb/msdb/models/review.py
+++ msdb/msdb/models/review.py
@@ -10,6 +10,6 @@ class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews", null=False, blank=False)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="reviews", null=False, blank=False)
     rating = models.IntegerField(choices=RATING_CHOICES, null=False, blank=False)
-    review = models.TextField()
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
