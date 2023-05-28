db81fef45e61119ff8940c144cbc32ff3e07f6e4
Kim Tranberg <mail@kimdt.tech>
2023-05-28 09:25:34 +0200

    Prepare settings to deploy on GCE

--- msdb/config/settings/production.py
+++ msdb/config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["kimdt.tech"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -59,7 +59,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Movies and Social DB <noreply@example.com>",
+    default="Movies and Social DB <noreply@kimdt.tech>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
